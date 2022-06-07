1ed6c6e36871f9a10677bc7674fce801c50e6fc2
Rik de Goede <rik.dgd@gmail.com>
2022-06-07 20:02:32 +0200

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,25 +1,31 @@
 /// <reference types="cypress" />
+import SessionHandler from '../../src/SessionHandler'
 
 describe('Login Test', () => {
 
   const correctUserName = 'Rik';
   const correctPassword = 'qwerty123';
+  const apiBaseUrl = 'http://localhost:8080/api/v1/login/';
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/');
     cy.wait(500);
   })
 
-  it('Can login', () => {
-    cy.visit('http://localhost:3000/login');
+  it('can fetch api', () => {
+    cy.request('GET', apiBaseUrl + correctUserName + '/' + correctPassword).as('login');
+    cy.get('@login').should((response) => {
+      expect(response.status).to.eq(200); // status 200 means data has been received succesfully
+    })
+  })
+
+  it('can login', () => {
+    cy.visit('/login');
     cy.get('#uname').type(correctUserName, { delay: 50 });
     cy.get('#pw').type(correctPassword, { delay: 50 });
     cy.get('#loginbtn').click();
 
-
-    cy.request('GET', 'http://localhost:8080/api/v1/login/' + correctUserName + '/' + correctPassword).as('login');
-    cy.get('@login').should((response) => {
-      expect(response.status).to.eq(200);
-    })
+    // check if userid is stored in session
+    expect(SessionHandler.GetUserId()).to.not.be.null
   })
 })
