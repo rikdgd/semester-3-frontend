ba2fd0b3cfe3e7bec659b8e373c57e235df9a216
Rik de Goede <rik.dgd@gmail.com>
2022-06-19 19:39:46 +0200

    removed unused pages, start on collection

---
+++ src/components/Collection/CollectionPage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CollectionPage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() { 
+        return ( 
+            <div className='container'>
+
+            </div>
+         );
+    }
+}
+ 
+export default CollectionPage;
